0e3f7c6dc971dab943b7467ec4e23552c58975bb
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-21 16:02:16 +0200

    missed file

---
+++ src/main/java/coffee_machine/controller/RegExp.java
@@ -0,0 +1,8 @@
+package coffee_machine.controller;
+
+public interface RegExp {
+	String REGEXP_DRINK_PARAM = "^drink_\\d+$";
+	String REGEXP_ADDON_IN_DRINK_PARAM = "^drink_\\d+addon_\\d+$";
+	String REGEXP_NUMBER = "\\d+";
+
+}
