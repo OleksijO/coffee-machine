30cb27ab692d10c9fb4f032581f22ea447f2a405
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-25 10:40:17 +0200

    Abstract connection became smarter. It rollbacks transaction if it wasn't committed when close method is called

--- src/main/java/coffee_machine/dao/impl/jdbc/AbstractConnectionImpl.java
+++ src/main/java/coffee_machine/dao/impl/jdbc/AbstractConnectionImpl.java
@@ -2,6 +2,7 @@ package coffee_machine.dao.impl.jdbc;
 
 import coffee_machine.dao.AbstractConnection;
 import coffee_machine.dao.exception.DaoException;
+import coffee_machine.i18n.message.key.error.DaoErrorKey;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
@@ -9,65 +10,73 @@ import java.sql.SQLException;
 
 public class AbstractConnectionImpl implements AbstractConnection {
 
-	private static final Logger logger = Logger.getLogger(AbstractConnectionImpl.class);
-	private Connection connection;
+    private static final Logger logger = Logger.getLogger(AbstractConnectionImpl.class);
+    private Connection connection;
+    private boolean transactionBegin = false;
+    private boolean transactionCommitted = false;
 
-	AbstractConnectionImpl(Connection connection) {
-		this.connection = connection;
-	}
+    AbstractConnectionImpl(Connection connection) {
+        this.connection = connection;
+    }
 
-	@Override
-	public void beginTransaction() {
-		try {
-			connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-			connection.setAutoCommit(false);
-		} catch (SQLException e) {
-			logErrorAndThrowDaoException("Can not set autocommit to false", e);
-		}
+    @Override
+    public void beginTransaction() {
+        try {
+            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            connection.setAutoCommit(false);
+            transactionBegin = true;
+        } catch (SQLException e) {
+            logErrorAndThrowDaoException(DaoErrorKey.CAN_NOT_BEGIN_TRANSACTION, e);
+        }
 
-	}
+    }
 
-	private void logErrorAndThrowDaoException(String message, Throwable e) {
-		logger.error(message, e);
-		throw new DaoException(message, e);
-	}
+    private void logErrorAndThrowDaoException(String message, Throwable e) {
+        logger.error(message, e);
+        throw new DaoException(message, e);
+    }
 
-	@Override
-	public void commitTransaction() {
-		try {
-			connection.commit();
-			connection.setAutoCommit(true);
-		} catch (SQLException e) {
-			logErrorAndThrowDaoException("Can not commit transaction.", e);
-		}
+    @Override
+    public void commitTransaction() {
+        try {
+            connection.commit();
+            connection.setAutoCommit(true);
+            transactionCommitted = true;
+        } catch (SQLException e) {
+            logErrorAndThrowDaoException(DaoErrorKey.CAN_NOT_COMMIT_TRANSACTION, e);
+        }
 
-	}
+    }
 
-	@Override
-	public void rollbackTransaction() {
-		try {
-			connection.rollback();
-			connection.setAutoCommit(true);
-		} catch (SQLException e) {
-			logErrorAndThrowDaoException("Can not rollback transaction.", e);
-		}
-	}
+    @Override
+    public void rollbackTransaction() {
+        try {
+            connection.rollback();
+            connection.setAutoCommit(true);
+            transactionCommitted = true;
+        } catch (SQLException e) {
+            logErrorAndThrowDaoException(DaoErrorKey.CAN_NOT_ROLLBACK_TRANSACTION, e);
+        }
+    }
 
-	@Override
-	public void close() {
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			logErrorAndThrowDaoException("Can not close SQL connection.", e);
-		}
-	}
+    @Override
+    public void close() {
+        try {
+            if (transactionBegin && !transactionCommitted) {
+                rollbackTransaction();
+            }
+            connection.close();
+        } catch (SQLException e) {
+            logErrorAndThrowDaoException(DaoErrorKey.CAN_NOT_CLOSE_CONNECTION, e);
+        }
+    }
 
-	Connection getConnection() {
-		return connection;
-	}
+    Connection getConnection() {
+        return connection;
+    }
 
-	void setConnection(Connection connection) {
-		this.connection = connection;
-	}
+    void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 
 }
