93fd4e0c205cc49f279adc35211e0978ba601d53
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-27 12:11:25 +0200

    DAO. Reduced number of messages

--- src/main/java/coffee_machine/i18n/message/key/EntityKey.java
+++
@@ -1,9 +0,0 @@
-package coffee_machine.i18n.message.key;
-
-public interface EntityKey {
-	String ACCOUNT = "error.dao.account.";
-	String USER = "error.dao.user.";
-	String ADDON = "error.dao.addon.";
-	String ADMIN = "error.dao.admin.";
-    String HISTORY_RECORD = "error.dao.history.record.";
-}
