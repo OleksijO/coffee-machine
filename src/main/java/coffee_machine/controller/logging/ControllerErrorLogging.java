465735e95ee6bc28096963dcc8ab0b7040905f73
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-12-02 13:29:42 +0200

    fixed refill page attributes

--- src/main/java/coffee_machine/controller/logging/ControllerErrorLogging.java
+++ src/main/java/coffee_machine/controller/logging/ControllerErrorLogging.java
@@ -1,47 +1,52 @@
 package coffee_machine.controller.logging;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
 import coffee_machine.exception.ApplicationException;
 import coffee_machine.logging.ApplicationErrorLogging;
 import coffee_machine.view.Attributes;
-import org.apache.log4j.Logger;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by oleksij.onysymchuk@gmail on 28.11.2016.
  */
 public interface ControllerErrorLogging extends ApplicationErrorLogging {
-    String MESSAGE_IN_CASE_OF_EMPTY = "";
+	String MESSAGE_IN_CASE_OF_EMPTY = "";
 
-    default void logError(Logger logger, String message, HttpServletRequest request, Exception e) {
-        logger.error(message + getRequestData(request), e);
-    }
+	default void logError(Logger logger, String message, HttpServletRequest request, Exception e) {
+		logger.error(message + getRequestData(request), e);
+	}
 
-    default void logError(Logger logger, HttpServletRequest request, Exception e) {
-        logError(logger, MESSAGE_IN_CASE_OF_EMPTY, request, e);
-    }
+	default void logError(Logger logger, HttpServletRequest request, Exception e) {
+		logError(logger, MESSAGE_IN_CASE_OF_EMPTY, request, e);
+	}
 
-    default void logApplicationError(Logger logger, HttpServletRequest request, ApplicationException e) {
-        logApplicationError(logger, e.getMessage(),
-                ((e.getAdditionalMessage() == null) ? e.getAdditionalMessage() : null) + getRequestData(request), e);
-    }
+	default void logApplicationError(Logger logger, HttpServletRequest request, ApplicationException e) {
+		logApplicationError(logger, e.getMessage(),
+				((e.getAdditionalMessage() == null) ? e.getAdditionalMessage() : null) + getRequestData(request), e);
+	}
 
-    default String getRequestData(HttpServletRequest request) {
-        StringBuilder messageBuilder = new StringBuilder().append("\nState:\t");
-        int userId = (int) request.getSession().getAttribute(Attributes.USER_ID);
-        if (userId > 0) {
-            messageBuilder.append("\tUser_ID=").append(userId);
-        }
-        int adminId = (int) request.getSession().getAttribute(Attributes.ADMIN_ID);
-        if (adminId > 0) {
-            messageBuilder.append("\tAdmin_ID=").append(userId);
-        }
-        messageBuilder
-                .append("\tRequest_URI=").append(request.getRequestURI())
-                .append("\tRequest_query=").append(request.getQueryString())
-                .append("\tUser_locale=").append(request.getSession().getAttribute(Attributes.USER_LOCALE));
+	default String getRequestData(HttpServletRequest request) {
+		StringBuilder messageBuilder = new StringBuilder().append("\nState:\t");
 
-        return messageBuilder.toString();
-    }
+		if (request.getSession().getAttribute(Attributes.USER_ID) != null) {
+			int userId = (int) request.getSession().getAttribute(Attributes.USER_ID);
+			if (userId > 0) {
+				messageBuilder.append("\tUser_ID=").append(userId);
+			}
+		}
+		if (request.getSession().getAttribute(Attributes.ADMIN_ID) != null) {
+			int adminId = (int) request.getSession().getAttribute(Attributes.ADMIN_ID);
+			if (adminId > 0) {
+				messageBuilder.append("\tAdmin_ID=").append(adminId);
+			}
+		}
+		messageBuilder.append("\tRequest_URI=").append(request.getRequestURI()).append("\tRequest_query=")
+				.append(request.getQueryString()).append("\tUser_locale=")
+				.append(request.getSession().getAttribute(Attributes.USER_LOCALE));
+
+		return messageBuilder.toString();
+	}
 
 }
