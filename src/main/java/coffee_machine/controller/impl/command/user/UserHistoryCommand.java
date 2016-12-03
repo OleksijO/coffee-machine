33a16c184139c7bc9241013c1b54cbc5e1a7e3bb
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-12-03 14:02:34 +0200

    Fixed history page title displaying

--- src/main/java/coffee_machine/controller/impl/command/user/UserHistoryCommand.java
+++ src/main/java/coffee_machine/controller/impl/command/user/UserHistoryCommand.java
@@ -3,13 +3,13 @@ package coffee_machine.controller.impl.command.user;
 import coffee_machine.controller.impl.command.CommandExecuteWrapper;
 import coffee_machine.service.HistoryRecordService;
 import coffee_machine.service.impl.HistoryRecordServiceImpl;
-import coffee_machine.view.Attributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static coffee_machine.view.Attributes.USER_ID;
+import static coffee_machine.i18n.message.key.GeneralKey.TITLE_USER_HISTORY;
+import static coffee_machine.view.Attributes.*;
 import static coffee_machine.view.PagesPaths.USER_HISTORY_PAGE;
 
 /**
@@ -26,8 +26,10 @@ public class UserHistoryCommand extends CommandExecuteWrapper {
 
     @Override
     protected String performExecute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         int userId = (int) request.getSession().getAttribute(USER_ID);
-        request.setAttribute(Attributes.USER_RECORD_HISTORY_LIST, historyRecordService.getAllByUserId(userId));
+        request.setAttribute(PAGE_TITLE, TITLE_USER_HISTORY);
+        request.setAttribute(USER_RECORD_HISTORY_LIST, historyRecordService.getAllByUserId(userId));
         return USER_HISTORY_PAGE;
     }
 
