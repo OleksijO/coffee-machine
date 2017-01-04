c71adf97e79a8fcff00b9d9a4d5d9e846b428278
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2017-01-04 13:46:10 +0200

    Refactored AuthFilter

--- src/main/java/coffee/machine/controller/security/AuthFilter.java
+++ src/main/java/coffee/machine/controller/security/AuthFilter.java
@@ -1,13 +1,15 @@
 package coffee.machine.controller.security;
 
-import coffee.machine.view.Attributes;
-import coffee.machine.view.PagesPaths;
 import org.apache.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
+import static coffee.machine.view.Attributes.ADMIN_ID;
+import static coffee.machine.view.Attributes.USER_ID;
+import static coffee.machine.view.PagesPaths.*;
 
 /**
  * This class represents security filter to restrict access of unauthorized users and admins to pages,
@@ -26,18 +28,33 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = ((HttpServletRequest) request);
         HttpSession session = req.getSession();
         String uri = req.getRequestURI();
-        Object adminId = session.getAttribute(Attributes.ADMIN_ID);
-        Object userId = session.getAttribute(Attributes.USER_ID);
+        Object adminId = session.getAttribute(ADMIN_ID);
+        Object userId = session.getAttribute(USER_ID);
 
-        if (((userId == null) && (uri.startsWith(PagesPaths.USER)) && (!uri.startsWith(PagesPaths.USER_REGISTER_PATH)))
-                || ((uri.startsWith(PagesPaths.ADMIN) && (adminId == null)))) {
-            req.getRequestDispatcher(PagesPaths.LOGIN_PATH).forward(request, response);
+        if (isAuthorized(uri, adminId, userId)) {
+            req.getRequestDispatcher(LOGIN_PATH).forward(request, response);
             logger.info(String.format(ACCESS_DENIED_LOG_MESSAGE_FORMAT, uri, userId, adminId));
             return;
         }
 
         chain.doFilter(request, response);
     }
+
+    private boolean isAuthorized(String uri, Object adminId, Object userId) {
+        return isUserAuthorized(uri, userId) || isAdminAuthorized(uri, adminId);
+    }
+
+    private boolean isUserAuthorized(String uri, Object userId) {
+        return (userId == null)
+                && (uri.startsWith(USER))
+                && (!uri.startsWith(USER_REGISTER_PATH));
+    }
+
+    private boolean isAdminAuthorized(String uri, Object adminId) {
+        return uri.startsWith(ADMIN)
+                && (adminId == null);
+    }
+
 
     @Override
     public void init(FilterConfig fConfig) throws ServletException {
